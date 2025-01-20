0b7205b1710ce6bb0f2c3b88ad2b0a8e4906f148
KevinAeiou <kevin.souzaeiou@outlook.com>
2025-01-20 14:38:35 -0300

    :truck: chore: fragmento fragment_questao_detalhada.xml para a pasta fragment

--- app/src/main/java/com/example/questoes/ui/fragment/QuestaoDetalhadaFragment.java
+++ app/src/main/java/com/example/questoes/ui/fragment/QuestaoDetalhadaFragment.java
@@ -1,4 +1,4 @@
-package com.example.questoes;
+package com.example.questoes.ui.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.questoes.R;
 
 /**
  * A simple {@link Fragment} subclass.
